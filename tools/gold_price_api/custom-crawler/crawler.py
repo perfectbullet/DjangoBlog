b6fb997716e90b9ae7ffeff69d451fea57545d04
cd-zhoujing <121531845@qq.com>
2023-08-06 00:13:41 +0800

    1. 增加docker环境 2. 增加获取 1970 至今的 数据 3. 数据处理，时间搓转化为date

--- tools/gold_price_api/custom-crawler/crawler.py
+++ tools/gold_price_api/custom-crawler/crawler.py
@@ -1,18 +1,23 @@
 import asyncio
 import json
 import os.path
+import sched
 import time
 from datetime import datetime
-import requests
+import re
+import random
 import pyppeteer
 import pytz
-from requests_html import AsyncHTMLSession, HTMLSession
-from data_utils import json_to_df, df_to_csv, save_json
+import requests
 from loguru import logger
+from requests_html import AsyncHTMLSession, HTMLSession
 
+from data_utils import json_to_df, df_to_csv, save_json, get_csv_names, concat_csv
+
+tz_info = pytz.timezone("America/New_York")
 
 class Crawler:
-    def __init__(self, name, url):
+    def __init__(self, name='', url=''):
         self.name = name
         self.url = url
 
@@ -40,11 +45,45 @@ class Crawler:
         print("File Success!")
         return 'ok'
 
+    def get_history_date(self):
+        """
+        获取自 1970-01-01 至 今的数据
+        按开始结束时间获取
+        https://fsapi.gold.org/api/goldprice/v11/chart/price/usd/oz/31881600000,63417600000?cache
+        @param start_date:
+        @param end_date:
+        @return:
+        """
+        year_1970 = 1970
+        year_now = 2023
+        # 美国东部时区的时间
+        res = []
+        for year in range(year_1970, year_now + 1):
+            start_date = datetime(year, 1, 1, 0, 0, 0, tzinfo=tz_info).timestamp() * 1000
+            end_date = datetime(year + 1, 1, 1, 0, 0, 0, tzinfo=tz_info).timestamp() * 1000
+            csv_path = self.get_by_date(start_date, end_date)
+            wait_seconds = random.randint(30, 90)
+            logger.info('get data from {} to {} + 1, wait_seconds {}'.format(year, year, wait_seconds))
+            res.append(csv_path)
+
+            time.sleep(wait_seconds)
+        return res
+
+    def get_by_date(self, start_tmsp: float, end_tmsp: float):
+        """
+        按开始结束时间获取
+        @param end_tmsp:
+        @param start_tmsp:
+        @return:
+        """
+        url_temp = "https://fsapi.gold.org/api/goldprice/v11/chart/price/usd/oz/{},{}?cache"
+        ok_url = url_temp.format(int(start_tmsp), int(end_tmsp))
+        return self.get_data_by_url(ok_url)
+
     def get_daily(self) -> str:
         """
         :return:
         """
-        tz_info = pytz.timezone("America/New_York")
         url_temp = "https://fsapi.gold.org/api/goldprice/v11/chart/price/usd/oz/{},{}?cache"
         # https://fsapi.gold.org/api/goldprice/v11/chart/price/usd/oz/1690719405089,1690805816601?cache
 
@@ -55,20 +94,51 @@ class Crawler:
         end_ts = us_eastern_dt.timestamp()
 
         start_ts = end_ts - 86402
-        start_date = datetime.fromtimestamp(start_ts).strftime('%Y%m%d%H%M%S')
+        start_date = datetime.fromtimestamp(start_ts, tz=tz_info).strftime('%Y%m%d%H%M%S')
 
         # send requests， 请求中的时间戳以毫秒计
         ok_url = url_temp.format(start_ts * 1e3, end_ts * 1e3)
+        return self.get_data_by_url(ok_url)
+
+    def get_data_by_url(self, url) -> str:
         session = HTMLSession()
-        resp: requests.Response = session.get(ok_url)
+        resp: requests.Response = session.get(url)
         json_data = resp.json()
 
         df = json_to_df(json_data)
+        parnt_str = r'https://fsapi.gold.org/api/goldprice/v11/chart/price/usd/oz/(\d*),(\d+)\?cache'
+        mt = re.match(parnt_str, url)
+        if mt.group(1):
+            start_date = datetime.fromtimestamp(int(mt.group(1)) / 1000).strftime('%Y%m%d%H%M%S')
+        else:
+            start_date = '19700101000000'
+        end_date = datetime.fromtimestamp(int(mt.group(2)) / 1000).strftime('%Y%m%d%H%M%S')
         csv_name = '{}_{}.csv'.format(start_date, end_date)
-        csv_path = os.path.join('../data/', csv_name)
+        csv_path = os.path.join('./data/', csv_name)
         save_json(json_data, csv_path.replace('.csv', '.json'))
         csv_path = df_to_csv(df, csv_path)
         return csv_path
+
+
+def time_worker():
+    crawler = Crawler('', '')
+    csv_path = crawler.get_daily()
+
+    csv_names = get_csv_names()
+    new_csv = concat_csv(csv_names)
+
+    logger.info(csv_path)
+    # 这里要再次使用, 不然要退出
+    loop_monitor()
+
+
+def loop_monitor():
+    s = sched.scheduler(time.time, time.sleep)  # 生成调度器
+    logger.info('loop_monitor')
+    # s.enter(3600 * 24, 1, time_worker, (crawler, ))
+    s.enter(3600, 1, time_worker, ())
+    s.run()
+    logger.info('s.run')
 
 
 if __name__ == '__main__':
@@ -83,6 +153,15 @@ if __name__ == '__main__':
 
     # Crawler
     # crawler = Crawler('2023-05-02_2023-07-28', url2)
+    # url = 'https://fsapi.gold.org/api/goldprice/v11/chart/price/usd/oz/1690644325034,1691249127107?cache'
+    # csv_path = crawler.get_data_by_url(url)
+    # print(csv_path)
+
+    # 获取历史数据
+    crawler = Crawler()
+    csv_paths = crawler.get_history_date()
+    print(csv_paths)
+
 
     # loop = asyncio.new_event_loop()
     # print("Running...")
@@ -91,5 +170,6 @@ if __name__ == '__main__':
     # print("Finished!")
     # res = task.result()
 
-    crawler = Crawler('', '')
-    crawler.get_daily()
+    # crawler.get_daily()
+
+    # loop_monitor()
--- tools/gold_price_api/custom-crawler/data_utils.py
+++ tools/gold_price_api/custom-crawler/data_utils.py
@@ -1,15 +1,14 @@
 import csv
 import json
 import os.path
+import re
 from datetime import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from pandas import DataFrame
-from datetime import datetime
-import time
 import pytz
+from pandas import DataFrame
 
 
 class TimeUtils:
@@ -86,34 +85,56 @@ def origin_data_deal(data):
     plt.show()
 
 
-def concat_csv(json_names, new_csv='new_csv.csv'):
+def concat_csv(csv_names):
     """
     合并csv, 并排序, 然后保存， 返回csv路径
     :return:
     """
+    tz_info = pytz.timezone("America/New_York")
+    us_eastern_dt = datetime.now(tz=tz_info)
+    date_str = us_eastern_dt.strftime('%Y%m%d%H%M%S')
+
+    new_csv = '{}_before.csv'.format(date_str)
     new_df = DataFrame()
-    for name in json_names:
+    for name in csv_names:
         # load data from csv
-        df_data = load_csv(os.path.join('../data/', name + '.csv'))
+        df_data = load_csv(name)
+        # csv 当前和新的数据合并
         new_df: DataFrame = pd.concat([new_df, df_data])
     new_df = new_df.sort_values(by='date')
-
-    # new_df.plot(x="date", y="price")
+    new_df['date'] = pd.to_datetime(new_df['date'], unit='ms')
+    new_df.plot(x="date", y="price")
     new_df.to_csv(new_csv, index=False)
     plt.show()
     return new_csv
 
 
+def get_csv_names(csv_dir='./data/'):
+    '''
+    csv_dir
+    @return: csv_names
+    '''
+
+    csv_names = []
+    for name in os.listdir(csv_dir):
+        pattern = re.compile(r'\d{14}_\d{14}\.csv')
+        mt = pattern.match(name)
+        if mt is not None:
+            csv_names.append(os.path.join(csv_dir, mt.group()))
+    return csv_names
+
+
 if __name__ == '__main__':
     json_path = '../data/2023-05-02_2023-07-28.json'
-    res = load_goldhub_data(json_path, chart_key='USD')
+    # res = load_goldhub_data(json_path, chart_key='USD')
 
     # gold price json names
-    json_names = ['2023-05-02_2023-07-28', '2013-08-01_2023-07-28', '2020-08-03_2023-07-28', '2022-08-01_2023-07-28',
-                  '2023-02-01_2023-07-28', ]
+    # json_names = ['2023-05-02_2023-07-28', '2013-08-01_2023-07-28', '2020-08-03_2023-07-28', '2022-08-01_2023-07-28',
+    #               '2023-02-01_2023-07-28', ]
     #
     # for name in json_names:
     #     csv_path = json_to_csv(os.path.join('../data/', name + '.json'), chart_key='USD')
     #     print('new csv path is ', csv_path)
-    new_csv = concat_csv(json_names, '../data/new_df.csv')
-    print(new_csv)
+    csv_names = get_csv_names()
+    new_csv = concat_csv(csv_names)
+    # print(new_csv)
